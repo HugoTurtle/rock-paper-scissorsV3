e58a8ec15306be2341ec2be62e7176527b282650
Hugo Perez <hugopereztm12309@gmail.com>
2024-04-24 18:46:15 -0400

    Create a function to randomly return a game move

--- main.js
+++ main.js
@@ -2,3 +2,12 @@
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
 }
+
+function getComputerChoice() {
+    const num = getRandomInt(3);
+
+        if (num == 1) return "rock";
+        else if (num == 2) return "paper";
+        else return "scissors";
+        
+}

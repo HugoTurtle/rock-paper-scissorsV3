55598b7b96b2af34c5eefd32258693528e3a0753
Hugo Perez <hugopereztm12309@gmail.com>
2024-05-03 18:36:21 -0400

    Allow the user to play with the click of a button

--- main.js
+++ main.js
@@ -90,15 +90,15 @@ buttonContainer.addEventListener(`click`, (Event) => {
 
     switch(target.id) {
         case 'rock':
-            
+            playRound(rock, getComputerChoice());
         break;
         
         case 'paper':
-
+            playRound(paper, getComputerChoice());
         break;
 
         case 'scissors': 
-
+            playRound(scissors, getComputerChoice());
         break;
     }
 })

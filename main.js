229d360738873e98af4b65719b7004737e6e5274
Hugo Perez <hugopereztm12309@gmail.com>
2024-05-03 19:51:59 -0400

    Display the winner after 5 games played
    Fixed a logic error to disable the buttons only after the 5th and last
    game is played. Previously the game would only play four rounds and skip
    the last round, preventing any further code from executing
    (e.g. determineWinner).

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ let computerScore = 0;
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
-let gamesPlayed = 1;
+let gamesPlayed = 0;
 
 const resultContainer = document.querySelector('#result-container');
 let score = document.createElement('p');
-const matchResult = document.createElement('p');
+let matchResult = document.createElement('p');
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
@@ -72,22 +72,17 @@ function playRound(humanChoice, computerChoice) {
 }
 
 function determineWinner(humanScore,computerScore) {
-    if(humanScore > computerScore) return "You win! Congratulation";
-    else if(computerScore >  humanScore) return "You lose. Please try again!";
-    else return "It's a tie! Please try again!";
+    if(humanScore > computerScore) matchResult.textContent = "You win! Congratulation";
+    else if(computerScore >  humanScore) matchResult.textContent = "You lose. Please try again!";
+    else matchResult.textContent = "It's a tie! Please try again!";
+
+    resultContainer.appendChild(matchResult);
 }
 
 const buttonContainer = document.querySelector("#button-container");
 
 buttonContainer.addEventListener(`click`, (Event) => {
     let target = Event.target;
-
-    if(gamesPlayed == 5) {
-        let buttons = document.querySelectorAll("button");
-        for(currentButton of buttons) {
-            currentButton.disabled = true;
-        }
-    }
 
     switch(target.id) {
         case 'rock':
@@ -105,4 +100,13 @@ buttonContainer.addEventListener(`click`, (Event) => {
             ++gamesPlayed;
         break;
     }
+
+    if(gamesPlayed == 5) {
+        determineWinner(humanScore,computerScore);
+        let buttons = document.querySelectorAll("button");
+        for(currentButton of buttons) {
+            currentButton.disabled = true;
+        }
+    }
+
 })
